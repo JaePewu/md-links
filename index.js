8ef5bc9b0c33df00360bb8588462ac31e6a6ffad
Jae <one.sweet.dreams@gmail.com>
2023-06-16 18:28:12 -0400

    Se implementan unas funciones para MD

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
+const path = require('path');
+
+function isAbsoluteRoute(route) {
+  try {
+    return path.isAbsolute(route);
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+  
+}
+// crear replace()
+// console.log(isAbsolute('C:/Users/onesw/OneDrive/Escritorio/Laboratoria/MD L/md-links/README.md'))
+
+module.exports = {
+  isAbsoluteRoute
+
 };
