c747239b9de298b6abd54bf9405235b21c118c68
Jae <one.sweet.dreams@gmail.com>
2023-07-06 01:45:49 -0400

    Se crean funcion para  entregar un array con validate getFormattedLinks

--- src/index.js
+++ src/index.js
@@ -28,16 +28,6 @@ const isValidRoute = (route) => {
   }
 };
 
-// function isValidRoute(route) {
-//   try {
-//     fs.accessSync(route); // Verificar la existencia del archivo o directorio
-//     return true;
-//   } catch (error) {
-//     console.log('Error:', error);
-//     return false;
-//   }
-// }
-
 
 /********* funtion para saber si es un archivo o un directorio ***********/
 const isFileInRoute = (route) => {
@@ -62,50 +52,36 @@ const isFileInRoute = (route) => {
 const readDirectory = (directoryRoute) => {
   const files = [];
 
-  const items = fs.readdirSync(directoryRoute);
+  const items = fs.readdirSync(directoryRoute);// Obtiene los elementos del directorio
 
-  if (items.length === 0) {
+  if (items.length === 0) {// Verifica si no hay archivos en el directorio
     throw new Error('No se encuentran archivos en el directorio');
   }
 
-  items.forEach((item) => {
-    const itemPath = path.join(directoryRoute, item);
+  items.forEach((item) => {// Recorre cada elemento del directorio
+    const itemPath = path.join(directoryRoute, item); // Obtiene la ruta completa del elemento
 
-    const stats = fs.statSync(itemPath);
+    const stats = fs.statSync(itemPath); // Obtiene información sobre el elemento (archivo o directorio)
 
-    if (stats.isDirectory()) {
+    if (stats.isDirectory()) {// Verifica si el elemento es un directorio
       try {
-        const subdirectoryFiles = readDirectory(itemPath);
-        files.push(...subdirectoryFiles);
+        files.push(readDirectory(itemPath));// Llamada recursiva para analizar el subdirectorio y obtener los archivos
       } catch (error) {
-        // Manejar el error si ocurre al leer el subdirectorio
-        console.error(`Error al leer el subdirectorio: ${itemPath}`, error);
+        // Maneja el error si ocurre al leer el subdirectorio(carpeta dentro de carpeta)
+        console.error(`Error al leer el Directorio: ${itemPath}`, error);
       }
-    } else if (path.extname(itemPath) === '.md') {
-      files.push(itemPath);
+    } else if (path.extname(itemPath) === '.md') {// Verifica si el elemento es un archivo Markdown
+      files.push(itemPath);// Agrega el archivo al arreglo de archivos encontrados
     }
   });
 
-  if (files.length === 0) {
+  if (files.length === 0) { // Verifica si no se encontraron archivos Markdown en el directorio
     throw new Error(`No se encontraron archivos Markdown en: ${directoryRoute}`);
   }
 
-  return files;
+  return files; // Retorna el arreglo con los archivos encontrados
 };
-console.log(readDirectory('C:\\Users\\onesw\\OneDrive\\Escritorio\\Laboratoria\\MD L\\md-links\\archivosDeEjemplo'));
-// function readDirectory(directoryRoute) {
-//   try {
-//     return fs.readdirSync(directoryRoute);
-//   } catch (error) {
-//     log('Error: No se encuentran archivos ', error);
-//   }
-// }
-
-//console.log(readDirectory('C:\\Users\\onesw\\OneDrive\\Escritorio\\Laboratoria\\JAESSTORE PROJECT\\jaesStore'));// devuelve los archivos
-// console.log(readDirectory('https://github.com/JaePewu/md-links#10-achicando-el-problema'));
-//console.log(readDirectory('C:\\Users\\onesw\\OneDrive\\Escritorio\\Laboratoria\\MD L\\md-links\\archivosDeEjemplo' ));
-// console.log(readDirectory('C:\\Users\\onesw\\OneDrive\\Escritorio\\Laboratoria\\MD L\\md-links\\archivosDeEjemplo\\Carpeta-Vacia'));
-
+// console.log(readDirectory('C:\\Users\\onesw\\OneDrive\\Escritorio\\Laboratoria\\MD L'));
 
 /* *********     funcion para extencion .md    ******************/
 const isMarkdown = (route) => {
@@ -121,18 +97,18 @@ const isMarkdown = (route) => {
 /***********   Funcion para leer archivos   ********/
 const readFile = (route) =>{
   return new Promise((resolve, reject) => {
-    fs.readFile(route, 'utf8', (error, content) => {
-            if (error) return reject(error);
-            return resolve(content);
+    fs.readFile(route, 'utf8', (error, content) => {// Lee el archivo con la codificación 'utf8'
+            if (error) return reject(error);// Rechaza la promesa si ocurre un error al leer el archivo
+            return resolve(content);// Resuelve la promesa con el contenido del archivo
         })
   })
   .then ((content) => {
-    log('Muestra el contenido del archivo', content);
-  // const links = getLinks(route, content);
-  //   log('Enlaces encontrados:', links);
+    log('Muestra el contenido del archivo', content);// Imprime el contenido del archivo si la promesa se resuelve
+    // const links = getLinks(route, content);
+    // log('Enlaces encontrados:', links);
   })
   .catch ((error) => {
-    log('Error al leer el archivo ', error);
+    log('Error al leer el archivo ', error);// Imprime un mensaje de error si la promesa es rechazada
   });
 }
 
@@ -170,18 +146,54 @@ const validateLinks = (links) => {
         resolve({ status: status, message: statusText }); // Resolver la promesa con el estado del enlace
       })
       .catch((error) => {
-        console.log(error + ' ERRRRORRRR');
-        // const status = '404'; // Not Found (No encontrado)
-        // const statusText = "Fail";
+        const status = error.response.status; // Estado de la respuesta HTTP en caso de error
+        const statusText = error.response.statusText; // Mensaje de estado en caso de error
 
         reject({ status, statusText, error }); // Rechazar la promesa con el estado del enlace y el error
       });
   });
 };
 
-  // console.log(validateLinks('https://curriculum.laboratoria.la/es/topics/javascript/arrays/array-proto')); // bueno
+   // console.log(validateLinks('https://curriculum.laboratoria.la/es/topics/javascript/arrays/array-proto')); // bueno
   // console.log(validateLinks('https://nodejs.dev/learn/the-package-json-guide')); // malo
 
+  /**---------------- Funcion para tomar lo que necesito de los links validados     --------------------- */
+  const getFormattedLinks = (links, validate) => {
+    return Promise.all( //dentro de Promise.all, se realiza un mapeo de cada enlace en links utilizando el método map
+      links.map(link => {
+        if (validate) {
+          // Validar el enlace si validate es true
+          return validateLinks(link.href)
+            .then(({ status, message }) => ({// => ({ ... }): Define una función de flecha que retorna un nuevo objeto. El objeto resultante es el objeto formateado con las propiedades deseadas.
+              // ({ status, message }): Utiliza la desestructuración de objetos para extraer las 
+              //propiedades status y message del objeto pasado como argumento
+              href: link.href,
+              text: link.text,
+              file: link.file,
+              status: status,
+              message: message,
+              ok: status >= 200 && status < 400 ? "OK" : "Fail"
+              //si el código de respuesta está en el rango 200-399, se establece como "OK"; de lo contrario, se establece como "Fail".
+            }))
+            .catch(error => ({
+              // Capturar el error de validación y crear el objeto con el enlace y el estado de error
+              href: link.href,
+              text: link.text,
+              file: link.file,
+              status: error.status,
+              ok: "Fail"
+            }));
+        } else {
+          // No validar el enlace, solo crear el objeto básico con la información del enlace
+          return Promise.resolve({
+            href: link.href,
+            text: link.text,
+            file: link.file
+          });
+        }
+      })
+    );
+  };
 
 module.exports = {
   isAbsoluteRoute,
@@ -192,5 +204,6 @@ module.exports = {
   isMarkdown,
   readFile,
   getLinks,
-  validateLinks
+  validateLinks,
+  getFormattedLinks
 };
